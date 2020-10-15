3384bfe61151c913cc2c7892f3f995a5ff7a809a
Huseyin ELMAS <hsynlms47@gmail.com>
2020-10-16 01:26:47 +0300

    new version 2.0.4

--- dist/theroom.js
+++ dist/theroom.js
@@ -1,5 +1,5 @@
 /*!
-* theroomjs v2.0.2
+* theroomjs v2.0.4
 * A vanilla javascript plugin that allows you to outline dom elements like web inspectors.
 * Works with Chrome, Firefox, Safari, Internet Explorer and Edge
 *
@@ -8,17 +8,16 @@
 (function (window, document, namespace) {
   var status = 'idle'
 
-  // default options
+  // defaults
   var options = {
     inspector: null,
     htmlClass: true,
     blockRedirection: false,
+    createInspector: false,
     excludes: []
   }
 
-  // gets the inspector instance
   var getInspector = function () {
-    // validation
     if (typeof options.inspector === 'string') {
       // if the provided inspector is a css selector, return the element
       var el = document.querySelector(options.inspector)
@@ -27,49 +26,54 @@
       else throw Error('inspector element not found')
     }
 
-    // validation
+    // eslint-disable-next-line
     if (options.inspector instanceof Element) {
       // if the provided inspector is a dom element, return it
       return el
     }
 
-    // validation failed
+    if (!options.inspector && options.createInspector) {
+      // create an inspector element
+      var _inspector = document.createElement('div')
+      _inspector.className = 'inspector-element'
+      document.body.appendChild(_inspector)
+      return _inspector
+    }
+
     throw Error('inspector must be a css selector or a DOM element')
   }
 
-  // gets the css selector for excluded list
   var getExclusionSelector = function () {
     return options.excludes.join(',')
   }
 
-  // merge options with defaults
   var applyOptions = function (opts) {
-    // validation
     if (typeof opts !== 'object') throw Error('options is expected to be an object')
 
     // merge
     for (var opt in opts) {
+      // eslint-disable-next-line
       if (opts.hasOwnProperty(opt)) {
         options[opt] = opts[opt]
       }
     }
   }
 
-  // event emitter
   var eventEmitter = function (event) {
     // hook event invocation
     eventController('hook', event)
 
-    // get target element
     var target = event.target
 
-    // validation --also skip inspector itself--
+    // validation --skip inspector element itself--
     if (!target || target === options.inspector) return
 
     // do not inspect excluded elements
     var excludedSelector = getExclusionSelector()
-    var excludedElements = Array.prototype.slice.call(document.querySelectorAll(excludedSelector))
-    if (excludedElements.indexOf(target) >= 0) return
+    if (excludedSelector) {
+      var excludedElements = Array.prototype.slice.call(document.querySelectorAll(excludedSelector))
+      if (excludedElements.indexOf(target) >= 0) return
+    }
 
     if (event.type === 'mouseover') {
       // get target element information
@@ -92,14 +96,12 @@
     eventController(event.type, target)
   }
 
-  // inspection engine
   var engine = function (type) {
-    // get HTML element
     var htmlEl = document.querySelector('html')
 
     if (type === 'start') {
-      // block redirection to another page
       if (options.blockRedirection === true) {
+        // block page redirection
         window.onbeforeunload = function () {
           return true
         }
@@ -109,72 +111,63 @@
       document.addEventListener('click', eventEmitter)
       document.addEventListener('mouseover', eventEmitter)
 
-      // add namespace as class to HTML element
+      // add namespace to HTML tag class list
       if (options.htmlClass === true) htmlEl.className += ' ' + namespace
 
       status = 'running'
     } else if (type === 'stop') {
-      // unbind event listeners
+      // remove binded event listeners
       document.removeEventListener('click', eventEmitter)
       document.removeEventListener('mouseover', eventEmitter)
 
-      // remove namespace from HTML element class list
+      // remove namespace from HTML tag class list
       if (options.htmlClass === true) htmlEl.className = htmlEl.className.replace(' ' + namespace, '')
 
-      // unblock redirection to another page
+      // remove blocking page redirection
       if (options.blockRedirection === true) window.onbeforeunload = undefined
 
       status = 'stopped'
     }
   }
 
-  // event executor
   var eventController = function (type, arg) {
-    // validations
     if (!options[type]) return
     if (typeof options[type] !== 'function') throw Error('event handler must be a function: ' + type)
 
-    // call the event and pass the argument
+    // call the event
     options[type].call(null, arg)
   }
 
-  // start inspection
   var start = function (opts) {
-    // merge provided options with defaults
-    applyOptions(opts)
+    if (opts) {
+      this.configure(opts)
+    }
 
     // get the inspector element
     options.inspector = getInspector()
 
-    // starting event
     eventController('starting')
 
     // start the inspection engine
     engine('start')
 
-    // started event
     eventController('started')
   }
 
-  // stop inspection
   var stop = function () {
-    // stopping event
     eventController('stopping')
 
     // stop the inspection engine
     engine('stop')
 
-    // stopped event
     eventController('stopped')
   }
 
-  // dynamically event binder
   var eventBinder = function (name, handler) {
-    // validations
     if (typeof name !== 'string') throw Error('event name is expected to be a string but got: ' + typeof name)
     if (typeof handler !== 'function') throw Error('event handler is not a function for: ' + name)
 
-    // update options
+    // update the event
     options[name] = handler
   }
 
@@ -183,6 +176,10 @@
     start: start,
     stop: stop,
     on: eventBinder,
+    configure: function (opts) {
+      // merge provided options with defaults
+      applyOptions(opts)
+    },
     status: function () {
       return status
     }
